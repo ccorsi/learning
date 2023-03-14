100c47351fb74e7d6a58f6760d01df3a03a2c4a0
Claudio Corsi <clcorsi@yahoo.com>
2023-03-14 02:13:54 -0400

    Created a single include file that contains all of the different loaders.

---
+++ utils/includes/loaders.h
@@ -0,0 +1,21 @@
+/**
+ * @file loaders.h
+ * @author Claudio Corsi (clcorsi@yahoo.com)
+ * @brief This include file will include all of the different loaders that are available
+ * @version 0.1
+ * @date 2023-03-14
+ *
+ * @copyright Copyright (c) 2023 Claudio Corsi
+ *
+ * @license MIT License https://raw.githubusercontent.com/ccorsi/learning/main/LICENSE
+ */
+
+#ifndef __LOADERS_H__
+#define __LOADERS_H__
+
+#include "loader.h"
+#include "vector_loader.h"
+#include "set_loader.h"
+#include "matrix_loader.h"
+
+#endif
